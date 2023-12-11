745fc49b3dd0b8a04a25be621996c96fd60e3b99
Dorian Eydoux <dorian.eydoux@icloud.com>
2023-12-11 07:48:07 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,44 @@
+NAME				=	push_swap
+
+LIBFT_DIR			=	libft
+SOURCES_DIR			=	sources
+INCLUDE_DIR			=	include
+OBJECTS_DIR			=	.objects
+
+SOURCES				=	push_swap.c
+
+CC					=	cc
+CFLAGS				=	-Wall -Wextra -Werror -MMD -I $(INCLUDE_DIR)
+RM					=	rm -rf
+MKDIR				=	mkdir -p
+
+LIBFT				=	$(LIBFT_DIR)/libft.a
+OBJECTS				=	$(addprefix $(OBJECTS_DIR)/,$(SOURCES:.c=.o))
+DEPENDENCIES		=	$(addprefix $(OBJECTS_DIR)/,$(SOURCES:.c=.d))
+
+all					:	$(NAME)
+
+-include 				$(DEPENDENCIES)
+
+$(LIBFT)			:
+	$(MAKE) -C $(@D) $(@F)
+
+$(OBJECTS_DIR)/%.o	:	$(SOURCES_DIR)/%.c
+	@$(MKDIR) $(@D)
+	$(CC) $(CFLAGS) -o $@ -c	$<
+
+$(NAME)				:	$(LIBFT) $(OBJECTS)
+	@$(MKDIR) $(@D)
+	$(CC) $(CFLAGS) -o $@ $^
+
+clean				:
+	$(MAKE) -C $(LIBFT_DIR) $@
+	$(RM) $(OBJECTS_DIR)
+
+fclean				:	clean
+	$(MAKE) -C $(LIBFT_DIR) $@
+	$(RM) $(NAME)
+
+re					:	fclean all
+
+.PHONY				:	all clean fclean re
---
+++ libft/sources/str/ft_split.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 11:21:57 by deydoux           #+#    #+#             */
+/*   Updated: 2023/11/13 16:24:44 by deydoux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	split_size(char const *s, char c)
+{
+	size_t	size;
+
+	size = 1;
+	while (*s)
+	{
+		if (*s != c && (!s[1] || s[1] == c))
+			size++;
+		s++;
+	}
+	return (size);
+}
+
+static int	create_str(char **split, size_t *i, char const *s, int end)
+{
+	split[*i] = ft_substr(s, 0, end);
+	if (!split[*i])
+	{
+		while ((*i)--)
+			free(split[*i]);
+		free(split);
+		return (0);
+	}
+	(*i)++;
+	return (1);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**split;
+	size_t	start;
+	size_t	s_len;
+	size_t	split_len;
+
+	if (!s)
+		return (NULL);
+	split = malloc(sizeof(char *) * split_size(s, c));
+	if (!split)
+		return (NULL);
+	s_len = 0;
+	start = 0;
+	split_len = 0;
+	while (s[s_len])
+	{
+		if (s[s_len++] == c)
+			start = s_len;
+		else if (!s[s_len] || s[s_len] == c)
+			if (!create_str(split, &split_len, s + start, s_len - start))
+				return (NULL);
+	}
+	split[split_len] = NULL;
+	return (split);
+}

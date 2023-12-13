8b8b7abc40c956a561e330cf397e562960907eb5
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-12-13 20:58:46 +0100

    Stage changes

--- sources/push_swap.c
+++ sources/push_swap.c
@@ -5,19 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/11 07:27:10 by deydoux           #+#    #+#             */
-/*   Updated: 2023/12/13 11:23:51 by deydoux          ###   ########.fr       */
+/*   Created: 2023/12/13 11:28:40 by deydoux           #+#    #+#             */
+/*   Updated: 2023/12/13 19:39:06 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static t_list	*parse_arg(char *arg)
+static t_list *parse_arg(char *arg, bool *error)
 {
+	t_list	*new;
 	int		sign;
-	int		n;
 	int		tmp_n;
+	int		n;
 
+	new = NULL;
 	sign = 1;
 	tmp_n = 0;
 	n = 0;
@@ -27,57 +29,45 @@ static t_list	*parse_arg(char *arg)
 	{
 		tmp_n = tmp_n * 10 + *arg++ - '0';
 		if (n != tmp_n / 10)
-			return (NULL);
+			break;
 		n = tmp_n;
 	}
 	if (*arg)
-		return (NULL);
-	return (ft_lstnew(&n));
+		*error = true;
 }
 
 static t_list	*parse_args(int argc, char **argv, bool *error)
 {
-	char	**splited_args;
-	size_t	i;
-	t_list	*arg;
+	char	**splitted_arg;
 	t_list	*stack;
+	size_t	i;
 
 	if (!argc)
 		return (NULL);
-	splited_args = ft_split(*argv, ' ');
-	if (!splited_args)
-		*error = true;
-	i = 0;
-	while (splited_args[i] && !*error)
+	splitted_arg = ft_split(*argv, ' ');
+	if (!splitted_arg)
 	{
-		arg = parse_arg(splited_args[i++]);
-		if (!arg)
-			*error = true;
-		ft_lstadd_back(&stack, arg);
+		*error = true;
+		return (NULL);
 	}
-	free_split(splited_args);
-	if (!error)
-		ft_lstadd_back(&stack, parse_args(argc - 1, argv + 1, error));
-	return (stack);
+	stack = NULL;
+	i = 0;
+	while (splitted_arg[i])
+		ft_lstadd_back()
 }
-
-#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
-	t_stacks	stacks;
 	bool		error;
+	t_stacks	stacks;
 
 	error = false;
 	stacks.a = parse_args(argc - 1, argv + 1, &error);
-	printf("%d", error);
 	if (error)
 	{
-		ft_lstclear(&(stacks.a), NULL);
 		ft_putstr_fd("Error\n", 2);
 		exit(EXIT_FAILURE);
 	}
 	stacks.b = NULL;
-	ft_lstclear(&(stacks.a), NULL);
 	exit(EXIT_SUCCESS);
 }

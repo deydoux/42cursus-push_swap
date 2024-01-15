370c9aa05ce9b3c7d949aca721044960892477d7
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-01-15 12:25:46 +0100

    Revert chunk_stack changes

--- sources/push_swap/chunk_stack.c
+++ sources/push_swap/chunk_stack.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 01:34:46 by deydoux           #+#    #+#             */
-/*   Updated: 2024/01/15 08:00:37 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/01/11 14:35:59 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,53 +20,31 @@ static bool	in_chunk(t_list *stack, size_t min, size_t max)
 	return (min <= index && index < max);
 }
 
-static int	get_direction(t_list *stack, size_t min, size_t max,
-	size_t rotations[2])
+static t_operation	get_rotation(t_list *stack, size_t min, size_t max)
 {
-	rotations[0] = 0;
+	size_t	rotations;
+	size_t	reverse_rotations;
+
+	rotations = 0;
 	while (stack && !in_chunk(stack, min, max))
 	{
-		rotations[0]++;
+		rotations++;
 		stack = stack->next;
 	}
 	if (!stack)
-		return (0);
-	if (!rotations[0])
-		return (1);
+		return (NULL);
+	if (!rotations)
+		return (rotate_a);
 	while (stack)
 	{
 		if (in_chunk(stack, min, max))
-			rotations[1] = 0;
-		rotations[1]++;
+			reverse_rotations = 0;
+		reverse_rotations++;
 		stack = stack->next;
 	}
-	if (rotations[1] < rotations[0])
-	{
-		rotations[0] = rotations[0];
-		return (-1);
-	}
-	return (1);
-}
-
-static t_operation	get_rotation(size_t n, int direction, t_stacks stacks)
-{
-	t_list	*stack;
-
-	if (direction == 1)
-		stack = simulate_rotations(n, *stacks.b);
-	else
-		stack = simulate_reverse_rotations(n, *stacks.b);
-
-}
-
-static bool	add_to_chunk(t_stacks stacks, size_t min, size_t max)
-{
-	int		direction;
-	size_t	rotations[2];
-
-	direction = get_direction(*stacks.a, min, max, rotations);
-	if (!direction)
-		return (true);
+	if (reverse_rotations < rotations)
+		return (reverse_rotate_a);
+	return (rotate_a);
 }
 
 void	chunk_stack(size_t chunks, size_t size, t_stacks stacks)

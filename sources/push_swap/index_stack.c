cd5031c689dc07f346edd566d42e39076d4f3abb
Dorian Eydoux <deydoux@student.42lyon.fr>
2024-01-24 18:22:09 +0100

    Optimize index_stack

--- sources/push_swap/index_stack.c
+++ sources/push_swap/index_stack.c
@@ -6,40 +6,40 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 14:34:00 by deydoux           #+#    #+#             */
-/*   Updated: 2024/01/11 14:37:33 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/01/24 18:21:31 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static t_list	*min_elem(t_list *stack)
+static t_elem	*min_elem(t_list *stack)
 {
-	t_list	*min;
+	t_elem	*min;
 
-	min = stack;
+	min = stack->content;
 	stack = stack->next;
 	while (stack)
 	{
-		if (((t_elem *)stack->content)->value < ((t_elem *)min->content)->value)
-			min = stack;
+		if (((t_elem *)stack->content)->value < min->value)
+			min = stack->content;
 		stack = stack->next;
 	}
 	return (min);
 }
 
-static t_list	*elem_greater_than(int value, t_list *stack)
+static t_elem	*elem_greater_than(int value, t_list *stack)
 {
-	t_list	*elem;
+	t_elem	*elem;
 
 	while (stack && ((t_elem *)stack->content)->value <= value)
 		stack = stack->next;
-	elem = stack;
+	elem = stack->content;
 	stack = stack->next;
 	while (stack)
 	{
-		if (((t_elem *)stack->content)->value < ((t_elem *)elem->content)->value
+		if (((t_elem *)stack->content)->value < elem->value
 			&& ((t_elem *)stack->content)->value > value)
-			elem = stack;
+			elem = stack->content;
 		stack = stack->next;
 	}
 	return (elem);
@@ -47,15 +47,15 @@ static t_list	*elem_greater_than(int value, t_list *stack)
 
 void	index_stack(t_list *stack, size_t size)
 {
-	t_list	*min;
+	t_elem	*min;
 	size_t	index;
 
 	min = min_elem(stack);
-	((t_elem *)min->content)->index = 0;
+	min->index = 0;
 	index = 1;
 	while (index < size)
 	{
-		min = elem_greater_than(((t_elem *)min->content)->value, stack);
-		((t_elem *)min->content)->index = index++;
+		min = elem_greater_than(min->value, stack);
+		min->index = index++;
 	}
 }
